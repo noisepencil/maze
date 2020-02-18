df8733c004e0c01dca38f1e04be835b635c6c2a7
Michael Andrews <Michael@Michaels-MacBook-Pro.local>
2020-02-18 11:42:41 -0500

    walls included in maze

--- com/noisepencil/maze/mazeRunner.java
+++ com/noisepencil/maze/mazeRunner.java
@@ -6,19 +6,21 @@ public class mazeRunner {
         System.out.println("MazeRunner 1.0");
 
         // Create some maze data
-        int[][] mazeData = new int[8][8];
+        int[][] mazeData = new int[10][10];
 
-        mazeData[0] = new int[] {0,1,0,1,0,1,1,1};
-        mazeData[1] = new int[] {0,1,1,1,0,0,0,1};
-        mazeData[2] = new int[] {0,0,0,1,1,0,1,1};
-        mazeData[3] = new int[] {1,1,1,0,1,0,1,0};
-        mazeData[4] = new int[] {0,0,1,0,1,1,1,1};
-        mazeData[5] = new int[] {1,0,1,1,0,0,1,0};
-        mazeData[6] = new int[] {1,0,0,1,0,0,1,0};
-        mazeData[7] = new int[] {1,1,1,1,1,1,1,1};
+        mazeData[0] = new int[] {0,0,0,0,0,0,0,0,0,0};
+        mazeData[1] = new int[] {0,1,1,1,1,1,1,1,1,0};
+        mazeData[2] = new int[] {0,1,0,0,1,0,0,1,0,0};
+        mazeData[3] = new int[] {0,1,0,1,1,0,0,1,0,0};
+        mazeData[4] = new int[] {0,0,0,1,0,1,1,1,1,0};
+        mazeData[5] = new int[] {1,1,1,1,0,1,0,1,0,0};
+        mazeData[6] = new int[] {0,0,0,0,1,1,0,1,1,0};
+        mazeData[7] = new int[] {0,0,1,1,1,0,0,0,1,0};
+        mazeData[8] = new int[] {0,0,1,0,1,0,1,1,1,0};
+        mazeData[9] = new int[] {0,0,1,0,0,0,0,0,0,0};
 
         // create a mazeRunner, initialize it with our data
-        Maze maze = new Maze(mazeData, 3, 0, 0, 1);
+        Maze maze = new Maze(mazeData, 5, 0, 9, 2);
 
         // print maze data
         maze.print();
