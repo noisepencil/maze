75727007b33b37d035a13cb9c270e31011863e07
Michael Andrews <Michael@Michaels-MacBook-Pro.local>
2020-02-18 17:41:32 -0500

    it works!

--- com/noisepencil/maze/Maze.java
+++ com/noisepencil/maze/Maze.java
@@ -1,7 +1,9 @@
 package com.noisepencil.maze;
 
+import java.util.ArrayList;
+
 public class Maze {
-    
+    public final int MAX_STEPS = 40;
     public Tile[][] tiles;
     public int numRows = 0;
     public int numCols= 0;
@@ -11,6 +13,24 @@ public class Maze {
     public int endRow = 0;
     public int endCol = 0;
 
+    public enum Direction {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST,
+        UNKNOWN
+    }
+    
+    public Direction direction = Direction.UNKNOWN;
+    public Direction cameFromDirection = Direction.UNKNOWN;
+
+    public Tile curTile;
+    public Tile prevTile;
+    public int steps = 0;
+    public Tile lastIntersection;
+
+    public ArrayList<Tile> path;
+
     public Maze(int[][] mazeData, int startRow, int startCol, int endRow, int endCol){
         numRows = mazeData.length;
         numCols = mazeData[0].length;
@@ -19,7 +39,7 @@ public class Maze {
         this.startCol = startCol;
         this.endRow = endRow;
         this.endCol = endCol;
-
+        
         tiles = new Tile[numRows][numCols];
 
         // load data into tile classes
@@ -40,93 +60,157 @@ public class Maze {
 
     public void walkMaze() {
 
-        int direction = 0;  // 0 = north, 1=east, 2=south, 3=west
+        path = new ArrayList<Tile>();
 
-        Tile curTile = tiles[this.startRow][this.startCol];
+        curTile = tiles[this.startRow][this.startCol];
+        prevTile = curTile;
         
         printTileData(curTile);
 
-        while (!amIatTheEnd(curTile)) {
-
-            if (canIGoNorth(curTile)) {
-                curTile = moveNorth(curTile);
-                printTileData(curTile);
-            }
-    
-            if (canIGoEast(curTile)) {
-                curTile = moveEast(curTile);
-                printTileData(curTile);
-            }
-
-            if (canIGoSouth(curTile)) {
-                curTile = moveSouth(curTile);
-                printTileData(curTile);
-            }
-    
-            if (canIGoWest(curTile)) {
-                curTile = moveWest(curTile);
-                printTileData(curTile);
-            }
-
+        while (!amIatTheEnd(curTile) && (steps <= MAX_STEPS)) {
+            makeAMove(curTile);
+            printTileData(curTile);
         }
-        System.out.println("I made it!");
+        System.out.println("");
+    }
+
+    public void makeAMove(Tile tile) {
+
+        if (isIntersection(tile)) {
+            lastIntersection = tile;
+        }
+
+        if (canIGoNorth(tile)) {
+            direction = Direction.NORTH;
+            curTile.beenNorth = true;
+            prevTile = curTile;
+            curTile = tileNorth(tile);
+            curTile.beenSouth = true;
+            System.out.println("moving north");
+
+        } else if (canIGoEast(tile)) {
+            direction = Direction.EAST;
+            curTile.beenEast = true;
+            prevTile = curTile;
+            curTile = tileEast(tile);
+            curTile.beenWest = true;
+            System.out.println("moving east");
+
+
+        }else if (canIGoSouth(tile)) {
+            direction = Direction.SOUTH;
+            curTile.beenSouth = true;
+            prevTile = curTile;
+            curTile = tileSouth(tile);
+            curTile.beenNorth = true;
+            System.out.println("moving south");
+
+
+        } else if (canIGoWest(tile)){
+            direction = Direction.WEST;
+            curTile.beenWest = true;
+            prevTile = curTile;
+            curTile = tileWest(tile);
+            curTile.beenEast = true;
+            System.out.println("moving west");
+
+        } else {
+            // if I get here I am at a dead end, I know this since all of the canIGoDirection functions
+            // prohibit me from backtracking as well as hitting walls..
+            // so its time to backtrack! 
+            // how far back do I need to go??
+            // I need to keep my path taken so far (in an array of tiles)?
+            // then walk back until I hit the last interection
+            // then take a fork i hadnt taken yet
+            System.out.println("Dead end! backing up to last intersection");
+            prevTile = curTile;
+            curTile = lastIntersection;
+        }
+
+        steps++;
+    }
+    
+    // are there more than one paths out of tile?
+    public boolean isIntersection(Tile tile) {
+        
+        int count = 0;
+
+        Tile neighbor = tileNorth(tile);
+        if (neighbor != null && !neighbor.isWall) count++;
+
+        neighbor = tileEast(tile);
+        if (neighbor != null && !neighbor.isWall) count++;
+
+        neighbor = tileSouth(tile);
+        if (neighbor != null && !neighbor.isWall) count++;
+
+        neighbor = tileWest(tile);
+        if (neighbor != null && !neighbor.isWall) count++;
+
+        return (count > 2);
+
     }
 
     public boolean amIatTheEnd(Tile tile) {
         if (tile.row == this.endRow && tile.col == this.endCol) {
+            System.out.println("I'm alive!!!!");
             return true;
         }
         return false;
     }
 
     public boolean canIGoNorth(Tile tile) {
-        if (tile.row == 0) {
+
+        if (tile.beenNorth) {
             return false;
         }
-        Tile tileAboveMe = tiles[tile.row-1][tile.col];
-       
-        if (tileAboveMe.isWall) {
+
+        Tile tileNorth = tileNorth(tile);
+        if (tileNorth == null) {
+            return false;
+        }
+
+        if (tileNorth.isWall || tileNorth == prevTile) {
             return false;
         }
 
         return true;
-    }
-
-    public Tile moveNorth(Tile tile) {
-        return tiles[tile.row-1][tile.col];
     }
 
     public boolean canIGoEast(Tile tile) {
-
         if (tile.col == numCols-1) {
             return false;
         }
-        if (tiles[tile.row][tile.col+1].isWall) {
+
+        if (tile.beenEast) {
+            return false;
+        }
+
+        Tile tileEast = tileEast(tile);
+
+        if (tileEast.isWall || tileEast == prevTile) {
             return false;
         }
 
         return true;
-    }
-
-    public Tile moveEast(Tile tile) {
-        return tiles[tile.row][tile.col+1];
     }
 
     public boolean canIGoSouth(Tile tile) {
         if (tile.row == numRows-1) {
             return false;
         }
-        Tile tileBelowMe = tiles[tile.row+1][tile.col];
+
+        if (tile.beenSouth) {
+            return false;
+        }
+
+        Tile tileSouth = tileSouth(tile);
        
-        if (tileBelowMe.isWall) {
+        if (tileSouth.isWall || tileSouth == prevTile) {
             return false;
         }
 
         return true;
-    }
-
-    public Tile moveSouth(Tile tile) {
-        return tiles[tile.row+1][tile.col];
     }
 
     public boolean canIGoWest(Tile tile) {
@@ -134,15 +218,46 @@ public class Maze {
         if (tile.col == 0) {
             return false;
         }
-        if (tiles[tile.row][tile.col-1].isWall) {
+
+        if (tile.beenWest) {
+            return false;
+        }
+
+        Tile tileWest = tileWest(tile);
+
+        if (tileWest.isWall || tileWest == prevTile) {
             return false;
         }
 
         return true;
     }
 
-    public Tile moveWest(Tile tile) {
-        return tiles[tile.row][tile.col-1];
+    public Tile tileNorth(Tile tile) {
+        if (tile.row > 0) {
+            return tiles[tile.row-1][tile.col];
+        }
+        return null;
+    }
+
+    public Tile tileEast(Tile tile) {
+        if (tile.col < numCols -1) {
+            return tiles[tile.row][tile.col+1];
+        }
+        return null;
+    }
+
+    public Tile tileSouth(Tile tile) {
+        if (tile.row < numRows -1) {
+            return tiles[tile.row+1][tile.col];
+        }
+        return null;
+    }
+
+    public Tile tileWest(Tile tile) {
+        if (tile.col > 0) {
+            return tiles[tile.row][tile.col-1];
+        }
+        return null;
     }
 
     public void printTileData(Tile tile) {
--- com/noisepencil/maze/Tile.java
+++ com/noisepencil/maze/Tile.java
@@ -9,12 +9,21 @@ public class Tile {
     public boolean isWall = false;
     public boolean isStart = false;
     public boolean isEnd = false;
-    
+
+    public boolean isForkNorth = false;
+    public boolean isForkEast = false;
+    public boolean isForkSouth = false;
+    public boolean isForkWest = false;
+
+    public boolean beenNorth = false;
+    public boolean beenEast = false;
+    public boolean beenSouth = false;
+    public boolean beenWest = false;
+
     // constructor
     public Tile(int row, int col, boolean isWall){
         this.row = row;
         this.col = col;
         this.isWall = isWall;
-    }
-
+    }    
 }
